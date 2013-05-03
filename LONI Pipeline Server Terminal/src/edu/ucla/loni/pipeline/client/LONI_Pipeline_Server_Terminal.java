93d7c7e458df2d0f843b495f9cbca9f8d6a4d58f
Kuan-Hao Su <ray60704@gmail.com>
2013-05-02 19:16:10 -0700

    Implement drag&drop area and an upload button

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/LONI_Pipeline_Server_Terminal.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/LONI_Pipeline_Server_Terminal.java
@@ -1,152 +1,184 @@
 package edu.ucla.loni.pipeline.client;
 
-import edu.ucla.loni.pipeline.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.event.dom.client.*;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
+// import com.google.gwt.widgetideas.client.ProgressBar;
+import org.moxieapps.gwt.uploader.client.Uploader;
+import org.moxieapps.gwt.uploader.client.events.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
- * Entry point classes define <code>onModuleLoad()</code>.
+ * Uploader Image Button with multiple file selection, drag and drop support, and GWT Progress Bars
+ * example of GWT SWF Uploader.
  */
 public class LONI_Pipeline_Server_Terminal implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
 
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+    public void onModuleLoad() {
+        final VerticalPanel progressBarPanel = new VerticalPanel();
+        // final Map<String, ProgressBar> progressBars = new LinkedHashMap<String, ProgressBar>();
+        final Map<String, Image> cancelButtons = new LinkedHashMap<String, Image>();
+        final Uploader uploader = new Uploader();
+        // uploader.setUploadURL("/DevNullUploadServlet")
+        uploader.setUploadURL("/FileUploadServlet")
+                .setButtonImageURL(GWT.getModuleBaseURL() + "resources/images/buttons/upload_new_version_button.png")
+                .setButtonWidth(133)
+                .setButtonHeight(22)
+                .setFileSizeLimit("50 MB")
+                .setButtonCursor(Uploader.Cursor.HAND)
+                .setButtonAction(Uploader.ButtonAction.SELECT_FILES)
+                .setFileQueuedHandler(new FileQueuedHandler() {
+                    public boolean onFileQueued(final FileQueuedEvent fileQueuedEvent) {
+                        // Create a Progress Bar for this file
+                        // final ProgressBar progressBar = new ProgressBar(0.0, 1.0, 0.0, new CancelProgressBarTextFormatter());
+                        // progressBar.setTitle(fileQueuedEvent.getFile().getName());
+                        // progressBar.setHeight("18px");
+                        // progressBar.setWidth("200px");
+                        // progressBars.put(fileQueuedEvent.getFile().getId(), progressBar);
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+                        // Add Cancel Button Image
+                        final Image cancelButton = new Image(GWT.getModuleBaseURL() + "resources/images/icons/cancel.png");
+                        cancelButton.setStyleName("cancelButton");
+                        cancelButton.addClickHandler(new ClickHandler() {
+                            public void onClick(ClickEvent event) {
+                                uploader.cancelUpload(fileQueuedEvent.getFile().getId(), false);
+                                // progressBars.get(fileQueuedEvent.getFile().getId()).setProgress(-1.0d);
+                                cancelButton.removeFromParent();
+                            }
+                        });
+                        cancelButtons.put(fileQueuedEvent.getFile().getId(), cancelButton);
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+                        // Add the Bar and Button to the interface
+                        // HorizontalPanel progressBarAndButtonPanel = new HorizontalPanel();
+                        // progressBarAndButtonPanel.add(progressBar);
+                        // progressBarAndButtonPanel.add(cancelButton);
+                        // progressBarPanel.add(progressBarAndButtonPanel);
 
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
+                        return true;
+                    }
+                })
+                .setUploadProgressHandler(new UploadProgressHandler() {
+                    public boolean onUploadProgress(UploadProgressEvent uploadProgressEvent) {
+                        // ProgressBar progressBar = progressBars.get(uploadProgressEvent.getFile().getId());
+                        // progressBar.setProgress(
+                        //         (double) uploadProgressEvent.getBytesComplete() / uploadProgressEvent.getBytesTotal()
+                        // );
+                        return true;
+                    }
+                })
+                .setUploadCompleteHandler(new UploadCompleteHandler() {
+                    public boolean onUploadComplete(UploadCompleteEvent uploadCompleteEvent) {
+                        cancelButtons.get(uploadCompleteEvent.getFile().getId()).removeFromParent();
+                        uploader.startUpload();
+                        return true;
+                    }
+                })
+                .setFileDialogStartHandler(new FileDialogStartHandler() {
+                    public boolean onFileDialogStartEvent(FileDialogStartEvent fileDialogStartEvent) {
+                        if (uploader.getStats().getUploadsInProgress() <= 0) {
+                            // Clear the uploads that have completed, if none are in process
+                            // progressBarPanel.clear();
+                            // progressBars.clear();
+                            cancelButtons.clear();
+                        }
+                        return true;
+                    }
+                })
+                .setFileDialogCompleteHandler(new FileDialogCompleteHandler() {
+                    public boolean onFileDialogComplete(FileDialogCompleteEvent fileDialogCompleteEvent) {
+                        if (fileDialogCompleteEvent.getTotalFilesInQueue() > 0) {
+                            if (uploader.getStats().getUploadsInProgress() <= 0) {
+                                uploader.startUpload();
+                            }
+                        }
+                        return true;
+                    }
+                })
+                .setFileQueueErrorHandler(new FileQueueErrorHandler() {
+                    public boolean onFileQueueError(FileQueueErrorEvent fileQueueErrorEvent) {
+                        Window.alert("Upload of file " + fileQueueErrorEvent.getFile().getName() + " failed due to [" +
+                                fileQueueErrorEvent.getErrorCode().toString() + "]: " + fileQueueErrorEvent.getMessage()
+                        );
+                        return true;
+                    }
+                })
+                .setUploadErrorHandler(new UploadErrorHandler() {
+                    public boolean onUploadError(UploadErrorEvent uploadErrorEvent) {
+                        cancelButtons.get(uploadErrorEvent.getFile().getId()).removeFromParent();
+                        Window.alert("Upload of file " + uploadErrorEvent.getFile().getName() + " failed due to [" +
+                                uploadErrorEvent.getErrorCode().toString() + "]: " + uploadErrorEvent.getMessage()
+                        );
+                        return true;
+                    }
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
+                })
+                .setUploadSuccessHandler(new UploadSuccessHandler() {
+                  public boolean onUploadSuccess(UploadSuccessEvent uploadSuccessEvent) {
+                    Window.alert("Response from server was: " + uploadSuccessEvent.getServerData());
+                    return true;
+                }
+            });
 
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
+        VerticalPanel verticalPanel = new VerticalPanel();
+        verticalPanel.add(uploader);
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
+        if (Uploader.isAjaxUploadWithProgressEventsSupported()) {
+            final Label dropFilesLabel = new Label("Drop Files Here");
+            dropFilesLabel.setStyleName("dropFilesLabel");
+            dropFilesLabel.addDragOverHandler(new DragOverHandler() {
+                public void onDragOver(DragOverEvent event) {
+                    if (!uploader.getButtonDisabled()) {
+                        dropFilesLabel.addStyleName("dropFilesLabelHover");
+                    }
+                }
+            });
+            dropFilesLabel.addDragLeaveHandler(new DragLeaveHandler() {
+                public void onDragLeave(DragLeaveEvent event) {
+                    dropFilesLabel.removeStyleName("dropFilesLabelHover");
+                }
+            });
+            dropFilesLabel.addDropHandler(new DropHandler() {
+                public void onDrop(DropEvent event) {
+                    dropFilesLabel.removeStyleName("dropFilesLabelHover");
 
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
+                    if (uploader.getStats().getUploadsInProgress() <= 0) {
+                        // progressBarPanel.clear();
+                        // progressBars.clear();
+                        cancelButtons.clear();
+                    }
 
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
+                    uploader.addFilesToQueue(Uploader.getDroppedFiles(event.getNativeEvent()));
+                    event.preventDefault();
+                }
+            });
+            verticalPanel.add(dropFilesLabel);
+        }
 
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
+        HorizontalPanel horizontalPanel = new HorizontalPanel();
+        horizontalPanel.add(verticalPanel);
+        // horizontalPanel.add(progressBarPanel);
+        horizontalPanel.setVerticalAlignment(HorizontalPanel.ALIGN_MIDDLE);
+        horizontalPanel.setCellHorizontalAlignment(uploader, HorizontalPanel.ALIGN_LEFT);
+        // horizontalPanel.setCellHorizontalAlignment(progressBarPanel, HorizontalPanel.ALIGN_RIGHT);
 
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
+        //noinspection GwtToHtmlReferences
+        RootPanel.get("uploadContainer").add(horizontalPanel);
+    }
 
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
-	}
+    // protected class CancelProgressBarTextFormatter extends ProgressBar.TextFormatter {
+        // @Override
+        // protected String getText(ProgressBar bar, double curProgress) {
+        //     if (curProgress < 0) {
+                // return "Cancelled";
+            // }
+            // return ((int) (100 * bar.getPercent())) + "%";
+        // }
+    // }
+
+
 }
