c4c2aa5c33ebd975dce1803af0b252c16512eb92
Jared <lindblom@cs.ucla.edu>
2013-05-03 11:20:11 -0700

    Modified Chart Redraw
    Removed TabSelectedHandler

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/LONI_Pipeline_Server_Terminal.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/LONI_Pipeline_Server_Terminal.java
@@ -1,8 +1,41 @@
 package edu.ucla.loni.pipeline.client;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.moxieapps.gwt.uploader.client.Uploader;
+import org.moxieapps.gwt.uploader.client.events.FileDialogCompleteEvent;
+import org.moxieapps.gwt.uploader.client.events.FileDialogCompleteHandler;
+import org.moxieapps.gwt.uploader.client.events.FileDialogStartEvent;
+import org.moxieapps.gwt.uploader.client.events.FileDialogStartHandler;
+import org.moxieapps.gwt.uploader.client.events.FileQueueErrorEvent;
+import org.moxieapps.gwt.uploader.client.events.FileQueueErrorHandler;
+import org.moxieapps.gwt.uploader.client.events.FileQueuedEvent;
+import org.moxieapps.gwt.uploader.client.events.FileQueuedHandler;
+import org.moxieapps.gwt.uploader.client.events.UploadCompleteEvent;
+import org.moxieapps.gwt.uploader.client.events.UploadCompleteHandler;
+import org.moxieapps.gwt.uploader.client.events.UploadErrorEvent;
+import org.moxieapps.gwt.uploader.client.events.UploadErrorHandler;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.DragLeaveEvent;
+import com.google.gwt.event.dom.client.DragLeaveHandler;
+import com.google.gwt.event.dom.client.DragOverEvent;
+import com.google.gwt.event.dom.client.DragOverHandler;
+import com.google.gwt.event.dom.client.DropEvent;
+import com.google.gwt.event.dom.client.DropHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.smartgwt.client.widgets.tab.TabSet;
 import com.smartgwt.client.widgets.tab.Tab;
+import com.smartgwt.client.widgets.tab.events.TabSelectedEvent;
+import com.smartgwt.client.widgets.tab.events.TabSelectedHandler;
 import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridField;
 import com.smartgwt.client.types.AutoFitWidthApproach;
@@ -119,7 +152,14 @@ public class LONI_Pipeline_Server_Terminal implements EntryPoint {
 		
 		Tab tabMemoryUsage = new Tab("Memory Usage");
 		
-		LONI_Chart memChart = new LONI_Chart("Memory");
+		final LONI_Chart memChart = new LONI_Chart("Memory");
+		tabMemoryUsage.addTabSelectedHandler(new TabSelectedHandler() {
+
+			@Override
+			public void onTabSelected(TabSelectedEvent event) {
+				memChart.redraw();
+			}});
+		
 		tabMemoryUsage.setPane(memChart);
 		
 		tabset.addTab(tabMemoryUsage);
@@ -127,6 +167,14 @@ public class LONI_Pipeline_Server_Terminal implements EntryPoint {
 		Tab tabThreadUsage = new Tab("Thread Usage");
 		
 		final LONI_Chart thrdChart = new LONI_Chart("Thread");
+		tabThreadUsage.addTabSelectedHandler(new TabSelectedHandler(){
+
+			@Override
+			public void onTabSelected(TabSelectedEvent event) {
+				thrdChart.redraw();
+				
+			}});
+		
 		tabThreadUsage.setPane(thrdChart);
 		
 		tabset.addTab(tabThreadUsage);
@@ -135,9 +183,132 @@ public class LONI_Pipeline_Server_Terminal implements EntryPoint {
 		tabset.addTab(tabPreferences);
 		
 		Tab tabUpload = new Tab("Upload");
-		tabUpload.addTabSelectedHandler(new UploadTabSelectedHandler(tabUpload));
+		createUploadTab(tabUpload);
 		tabset.addTab(tabUpload);
 		
 		tabset.draw();
+	}
+	
+	public void createUploadTab(Tab tabUpload) {
+		final VerticalPanel progressBarPanel = new VerticalPanel();  
+		final Map<String, Image> cancelButtons = new LinkedHashMap<String, Image>();  
+		final Uploader uploader = new Uploader();  
+		uploader.setUploadURL("/DevNullUploadServlet")  
+		.setButtonImageURL(GWT.getModuleBaseURL() + "resources/images/buttons/upload_new_version_button.png")  
+		.setButtonWidth(133)  
+		.setButtonHeight(22)  
+		.setFileSizeLimit("50 MB")  
+		.setButtonCursor(Uploader.Cursor.HAND)  
+		.setButtonAction(Uploader.ButtonAction.SELECT_FILES)  
+		.setFileQueuedHandler(new FileQueuedHandler() {  
+			public boolean onFileQueued(final FileQueuedEvent fileQueuedEvent) {  
+				// Add Cancel Button Image  
+				final Image cancelButton = new Image(GWT.getModuleBaseURL() + "resources/images/icons/cancel.png");  
+				cancelButton.setStyleName("cancelButton");  
+				cancelButton.addClickHandler(new ClickHandler() {  
+					public void onClick(ClickEvent event) {  
+						uploader.cancelUpload(fileQueuedEvent.getFile().getId(), false);  
+						cancelButton.removeFromParent();  
+					}  
+				});  
+				cancelButtons.put(fileQueuedEvent.getFile().getId(), cancelButton);  
+
+				// Add the Bar and Button to the interface  
+				HorizontalPanel progressBarAndButtonPanel = new HorizontalPanel();  
+				progressBarAndButtonPanel.add(cancelButton);  
+				progressBarPanel.add(progressBarAndButtonPanel);  
+
+				return true;  
+			}  
+		})    
+		.setUploadCompleteHandler(new UploadCompleteHandler() {  
+			public boolean onUploadComplete(UploadCompleteEvent uploadCompleteEvent) {  
+				cancelButtons.get(uploadCompleteEvent.getFile().getId()).removeFromParent();  
+				uploader.startUpload();  
+				return true;  
+			}  
+		})  
+		.setFileDialogStartHandler(new FileDialogStartHandler() {  
+			public boolean onFileDialogStartEvent(FileDialogStartEvent fileDialogStartEvent) {  
+				if (uploader.getStats().getUploadsInProgress() <= 0) {  
+					// Clear the uploads that have completed, if none are in process  
+					progressBarPanel.clear();  
+					cancelButtons.clear();  
+				}  
+				return true;  
+			}  
+		})  
+		.setFileDialogCompleteHandler(new FileDialogCompleteHandler() {  
+			public boolean onFileDialogComplete(FileDialogCompleteEvent fileDialogCompleteEvent) {  
+				if (fileDialogCompleteEvent.getTotalFilesInQueue() > 0) {  
+					if (uploader.getStats().getUploadsInProgress() <= 0) {  
+						uploader.startUpload();  
+					}  
+				}  
+				return true;  
+			}  
+		})  
+		.setFileQueueErrorHandler(new FileQueueErrorHandler() {  
+			public boolean onFileQueueError(FileQueueErrorEvent fileQueueErrorEvent) {  
+				Window.alert("Upload of file " + fileQueueErrorEvent.getFile().getName() + " failed due to [" +  
+						fileQueueErrorEvent.getErrorCode().toString() + "]: " + fileQueueErrorEvent.getMessage()  
+						);  
+				return true;  
+			}  
+		})  
+		.setUploadErrorHandler(new UploadErrorHandler() {  
+			public boolean onUploadError(UploadErrorEvent uploadErrorEvent) {  
+				cancelButtons.get(uploadErrorEvent.getFile().getId()).removeFromParent();  
+				Window.alert("Upload of file " + uploadErrorEvent.getFile().getName() + " failed due to [" +  
+						uploadErrorEvent.getErrorCode().toString() + "]: " + uploadErrorEvent.getMessage()  
+						);  
+				return true;  
+			}  
+		});  
+
+		VerticalPanel verticalPanel = new VerticalPanel();  
+		verticalPanel.add(uploader);  
+
+		if (Uploader.isAjaxUploadWithProgressEventsSupported()) {  
+			final Label dropFilesLabel = new Label("Drop Files Here");  
+			dropFilesLabel.setStyleName("dropFilesLabel");  
+			dropFilesLabel.addDragOverHandler(new DragOverHandler() {  
+				public void onDragOver(DragOverEvent event) {  
+					if (!uploader.getButtonDisabled()) {  
+						dropFilesLabel.addStyleName("dropFilesLabelHover");  
+					}  
+				}  
+			});  
+			dropFilesLabel.addDragLeaveHandler(new DragLeaveHandler() {  
+				public void onDragLeave(DragLeaveEvent event) {  
+					dropFilesLabel.removeStyleName("dropFilesLabelHover");  
+				}  
+			});  
+			dropFilesLabel.addDropHandler(new DropHandler() {  
+				public void onDrop(DropEvent event) {  
+					dropFilesLabel.removeStyleName("dropFilesLabelHover");  
+
+					if (uploader.getStats().getUploadsInProgress() <= 0) {  
+						progressBarPanel.clear();  
+						cancelButtons.clear();  
+					}  
+
+					uploader.addFilesToQueue(Uploader.getDroppedFiles(event.getNativeEvent()));  
+					event.preventDefault();  
+				}  
+			});  
+			verticalPanel.add(dropFilesLabel);  
+		}  
+
+		HorizontalPanel horizontalPanel = new HorizontalPanel();  
+		horizontalPanel.add(verticalPanel);  
+		horizontalPanel.add(progressBarPanel);  
+		horizontalPanel.setVerticalAlignment(HorizontalPanel.ALIGN_MIDDLE);  
+		horizontalPanel.setCellHorizontalAlignment(uploader, HorizontalPanel.ALIGN_LEFT);  
+		horizontalPanel.setCellHorizontalAlignment(progressBarPanel, HorizontalPanel.ALIGN_RIGHT);
+		
+		VLayout uploadLayout = new VLayout();
+		uploadLayout.addMember(horizontalPanel);
+		tabUpload.setPane(uploadLayout);
 	}
 }
