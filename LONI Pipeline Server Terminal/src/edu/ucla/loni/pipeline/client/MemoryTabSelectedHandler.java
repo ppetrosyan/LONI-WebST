11a481922f54e1d45fdbc26c6d1bb0d226165143
Jared <lindblom@cs.ucla.edu>
2013-05-03 02:06:46 -0700

    Added Two Charts using Moxie HighCharts

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/MemoryTabSelectedHandler.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/MemoryTabSelectedHandler.java
@@ -1,5 +1,9 @@
 package edu.ucla.loni.pipeline.client;
 
+import org.moxieapps.gwt.highcharts.client.Chart;
+import org.moxieapps.gwt.highcharts.client.Series;
+
+import com.smartgwt.client.widgets.layout.VLayout;
 import com.smartgwt.client.widgets.tab.Tab;
 import com.smartgwt.client.widgets.tab.events.TabSelectedEvent;
 import com.smartgwt.client.widgets.tab.events.TabSelectedHandler;
@@ -7,14 +11,26 @@ import com.smartgwt.client.widgets.tab.events.TabSelectedHandler;
 public class MemoryTabSelectedHandler implements TabSelectedHandler {
 
 	private Tab tabMemoryUsage;
-	
-	public MemoryTabSelectedHandler(Tab tabMemory) {
+
+	public MemoryTabSelectedHandler(Tab tabMemoryUsage) {
 		this.tabMemoryUsage = tabMemoryUsage;
 	}
-	
+
 	@Override
 	public void onTabSelected(TabSelectedEvent event) {
+		Chart chart = new Chart()
+		.setType(Series.Type.SPLINE)
+		.setChartTitleText("Lawn Tunnels")
+		.setMarginRight(10);
 
+		Series series = chart.createSeries()
+				.setName("Moles per Yard")
+				.setPoints(new Number[] { 163, 203, 276, 408, 547, 729, 628 });
+		chart.addSeries(series);
+
+		VLayout uploadLayout = new VLayout();
+		uploadLayout.addMember(chart);
+		tabMemoryUsage.setPane(uploadLayout);
 	}
 
 }
--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/ThreadTabSelectedHandler.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/ThreadTabSelectedHandler.java
@@ -1,5 +1,9 @@
 package edu.ucla.loni.pipeline.client;
 
+import org.moxieapps.gwt.highcharts.client.Chart;
+import org.moxieapps.gwt.highcharts.client.Series;
+
+import com.smartgwt.client.widgets.layout.VLayout;
 import com.smartgwt.client.widgets.tab.Tab;
 import com.smartgwt.client.widgets.tab.events.TabSelectedEvent;
 import com.smartgwt.client.widgets.tab.events.TabSelectedHandler;
@@ -14,7 +18,19 @@ public class ThreadTabSelectedHandler implements TabSelectedHandler {
 	
 	@Override
 	public void onTabSelected(TabSelectedEvent event) {
+		Chart chart = new Chart()
+		.setType(Series.Type.SPLINE)
+		.setChartTitleText("Lawn Tunnels")
+		.setMarginRight(10);
 
+		Series series = chart.createSeries()
+				.setName("Moles per Yard")
+				.setPoints(new Number[] { 163, 203, 276, 408, 547, 729, 628 });
+		chart.addSeries(series);
+
+		VLayout uploadLayout = new VLayout();
+		uploadLayout.addMember(chart);
+		tabThreadUsage.setPane(uploadLayout);
 	}
 
 }
