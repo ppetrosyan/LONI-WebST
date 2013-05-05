043a7096910033a8f42d12b5a6a83292778105e4
Jared <lindblom@cs.ucla.edu>
2013-05-05 12:26:57 -0700

    Client Receives JSON response from server, displays JSON to user.

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/server/FileUploadServlet.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/server/FileUploadServlet.java
@@ -7,17 +7,13 @@ import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-import javax.annotation.RegEx;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class FileUploadServlet extends HttpServlet {
 
@@ -28,7 +24,7 @@ public class FileUploadServlet extends HttpServlet {
 
 		try {
 			ServletFileUpload upload = new ServletFileUpload();
-			resp.setContentType("text/xml");
+			resp.setContentType("text/json");
 
 			FileItemIterator iterator = upload.getItemIterator(req);
 			while (iterator.hasNext()) {
@@ -38,6 +34,7 @@ public class FileUploadServlet extends HttpServlet {
 				XMLSerializer xmlSerializer = new XMLSerializer();
 				JSON json = xmlSerializer.readFromStream(stream);
 
+				resp.setStatus(HttpServletResponse.SC_OK);
 				resp.getWriter().print(json.toString(2));
 				resp.flushBuffer();
 			}
