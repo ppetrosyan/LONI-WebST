fc533cbdf33c1700268f0e036904d4fc8ec4b92f
Troy Tabrilla <troytabrilla@gmail.com>
2013-05-04 21:30:00 -0700

    Added chart statistics and checkboxes for memory chart Added MemoryStatistics class Moved timer from LineChartPanel to LONI_Chart Removed randomized data for Initial Memory and Max Memory

---
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/MemoryStatistics.java
@@ -0,0 +1,81 @@
+package edu.ucla.loni.pipeline.client;
+
+public class MemoryStatistics {
+
+	private int initMemMB;
+	private int usedMemMB;
+	private int commMemMB;
+	private int maxMemMB;
+	
+	private int usedCommMemPercent;
+	private int usedMaxMemPercent;
+	private int commMaxMemPercent;
+	
+	public MemoryStatistics() {
+		setInitMemMB(0);
+		setUsedMemMB(0);
+		setCommMemMB(0);
+		setMaxMemMB(0);
+		
+		setUsedCommMemPercent(0);
+		setUsedMaxMemPercent(0);
+		setCommMaxMemPercent(0);
+	}
+
+	public int getInitMemMB() {
+		return initMemMB;
+	}
+
+	public void setInitMemMB(int initMemMB) {
+		this.initMemMB = initMemMB;
+	}
+
+	public int getUsedMemMB() {
+		return usedMemMB;
+	}
+
+	public void setUsedMemMB(int usedMemMB) {
+		this.usedMemMB = usedMemMB;
+	}
+
+	public int getCommMemMB() {
+		return commMemMB;
+	}
+
+	public void setCommMemMB(int commMemMB) {
+		this.commMemMB = commMemMB;
+	}
+
+	public int getMaxMemMB() {
+		return maxMemMB;
+	}
+
+	public void setMaxMemMB(int maxMemMB) {
+		this.maxMemMB = maxMemMB;
+	}
+
+	public int getUsedCommMemPercent() {
+		return usedCommMemPercent;
+	}
+
+	public void setUsedCommMemPercent(int usedCommMemPercent) {
+		this.usedCommMemPercent = usedCommMemPercent;
+	}
+
+	public int getUsedMaxMemPercent() {
+		return usedMaxMemPercent;
+	}
+
+	public void setUsedMaxMemPercent(int usedMaxMemPercent) {
+		this.usedMaxMemPercent = usedMaxMemPercent;
+	}
+
+	public int getCommMaxMemPercent() {
+		return commMaxMemPercent;
+	}
+
+	public void setCommMaxMemPercent(int commMaxMemPercent) {
+		this.commMaxMemPercent = commMaxMemPercent;
+	}
+	
+}
