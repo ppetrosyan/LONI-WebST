0064688eab8a800d1689b49845a8ad230377a8dd
Jared <lindblom@cs.ucla.edu>
2013-05-12 13:45:04 -0700

    Responds to Configuration or Simulated Data

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/server/FileUploadServlet.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/server/FileUploadServlet.java
@@ -3,8 +3,10 @@ package edu.ucla.loni.pipeline.server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
+import java.util.Hashtable;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -19,6 +21,10 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 
@@ -33,10 +39,16 @@ import com.google.appengine.api.files.FileWriteChannel;
 
 public class FileUploadServlet extends HttpServlet {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 5842494091385532249L;
+	private Hashtable<XMLType, String> xmlPathTable;
+	
+	private enum XMLType {
+	    CONFIGURATION, SIMULATEDDATA
+	}
+	
+	public FileUploadServlet() {
+		xmlPathTable = new Hashtable<XMLType, String>();
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -75,8 +87,6 @@ public class FileUploadServlet extends HttpServlet {
 					String rootTag = doc.getDocumentElement().getNodeName();
 
 					if(rootTag.equalsIgnoreCase("LONIConfigurationData")) {
-						item.getInputStream().reset();
-
 						// Get a file service
 						FileService fileService = FileServiceFactory.getFileService();
 
@@ -86,11 +96,20 @@ public class FileUploadServlet extends HttpServlet {
 						// Open a channel to write to it
 						boolean lock = true;
 						FileWriteChannel writeChannel = fileService.openWriteChannel(file, lock);
+						
+						DOMSource domSource = new DOMSource(doc);
 
+						StringWriter writer = new StringWriter();
+						StreamResult streamResult = new StreamResult(writer);
+						
+						TransformerFactory transformerFactory = TransformerFactory.newInstance();
+						Transformer transformer = transformerFactory.newTransformer();
+						transformer.transform(domSource, streamResult);
+						
 						// Different standard Java ways of writing to the channel
 						// are possible. Here we use a PrintWriter:
 						PrintWriter out = new PrintWriter(Channels.newWriter(writeChannel, "UTF8"));
-						out.println("test...");
+						out.println(writer.toString());
 
 						// Close without finalizing and save the file path for writing later
 						out.close();
@@ -98,10 +117,11 @@ public class FileUploadServlet extends HttpServlet {
 						// Now finalize
 						writeChannel.closeFinally();
 						
-						String path = file.getFullPath();
+						// Add Path to Hashtable
+						xmlPathTable.put(XMLType.CONFIGURATION, file.getFullPath());
 
-						// Write more to the file in a separate request:
-						file = new AppEngineFile(path);
+						// Get File from Blobstore
+						file = new AppEngineFile(xmlPathTable.get(XMLType.CONFIGURATION));
 
 						// Later, read from the file using the file API
 						lock = false; // Let other people read at the same time
@@ -114,16 +134,58 @@ public class FileUploadServlet extends HttpServlet {
 
 						readChannel.close();
 
-						// Now read from the file using the Blobstore API
-						/*BlobKey blobKey = fileService.getBlobKey(file);
-						BlobstoreService blobStoreService = BlobstoreServiceFactory.getBlobstoreService();
-						String segment = new String(blobStoreService.fetchData(blobKey, 30, 40));*/
-
 						resp.setStatus(HttpServletResponse.SC_OK);
 						resp.getWriter().print("File Uploaded Successfully, detected Configuration Data." + line);
 						resp.flushBuffer();
 					}
 					else if(rootTag.equalsIgnoreCase("LONISimulatedData")){
+						// Get a file service
+						FileService fileService = FileServiceFactory.getFileService();
+
+						// Create a new Blob file with mime-type "text/plain"
+						AppEngineFile file = fileService.createNewBlobFile("text/plain");
+
+						// Open a channel to write to it
+						boolean lock = true;
+						FileWriteChannel writeChannel = fileService.openWriteChannel(file, lock);
+						
+						DOMSource domSource = new DOMSource(doc);
+
+						StringWriter writer = new StringWriter();
+						StreamResult streamResult = new StreamResult(writer);
+						
+						TransformerFactory transformerFactory = TransformerFactory.newInstance();
+						Transformer transformer = transformerFactory.newTransformer();
+						transformer.transform(domSource, streamResult);
+						
+						// Different standard Java ways of writing to the channel
+						// are possible. Here we use a PrintWriter:
+						PrintWriter out = new PrintWriter(Channels.newWriter(writeChannel, "UTF8"));
+						out.println(writer.toString());
+
+						// Close without finalizing and save the file path for writing later
+						out.close();
+						
+						// Now finalize
+						writeChannel.closeFinally();
+						
+						// Add Path to Hashtable
+						xmlPathTable.put(XMLType.SIMULATEDDATA, file.getFullPath());
+
+						// Get File from Blobstore
+						file = new AppEngineFile(xmlPathTable.get(XMLType.SIMULATEDDATA));
+
+						// Later, read from the file using the file API
+						lock = false; // Let other people read at the same time
+						FileReadChannel readChannel = fileService.openReadChannel(file, false);
+
+						// Again, different standard Java ways of reading from the channel.
+						BufferedReader reader =
+								new BufferedReader(Channels.newReader(readChannel, "UTF8"));
+						String line = reader.readLine();
+
+						readChannel.close();
+						
 						resp.setStatus(HttpServletResponse.SC_OK);
 						resp.getWriter().print("File Uploaded Successfully, detected Simulated Data.");
 						resp.flushBuffer();
