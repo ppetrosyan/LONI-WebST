c2e0877a08177477a38101a17bd0f15e42e5690b
Jared <lindblom@cs.ucla.edu>
2013-05-15 15:50:39 -0700

    LONIDataRequester: Tabs refresh on Upload Success

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/Upload/Uploaders/LONIFileUploader.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/Upload/Uploaders/LONIFileUploader.java
@@ -5,12 +5,12 @@ import java.util.Map;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-import edu.ucla.loni.pipeline.client.Requesters.XMLDataServiceAsync;
+import edu.ucla.loni.pipeline.client.Requesters.LONIDataRequester;
 
 public class LONIFileUploader extends LONIUploader {
-
-	public LONIFileUploader(Map<String, Image> cancelButtons, VerticalPanel progressBarPanel, XMLDataServiceAsync xmlDataService) {
-		super(cancelButtons, progressBarPanel, xmlDataService);
+	
+	public LONIFileUploader(Map<String, Image> cancelButtons, VerticalPanel progressBarPanel, LONIDataRequester dataRequester) {
+		super(cancelButtons, progressBarPanel, dataRequester);
 		
 		setUploadURL("/FileUploadServlet");
 	}
