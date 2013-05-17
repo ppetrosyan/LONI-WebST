7dc30c6d541f36cf6f0cd98a410c5fe651b6aa45
Deepak Ananth Rama <maildeepakar@gmail.com>
2013-05-17 12:09:10 -0700

    Refactored code in main GUI class, added layouts and buttons, fixed display issues in Pref

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/Requesters/RefreshAllTabs/LONIDataRequester.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/Requesters/RefreshAllTabs/LONIDataRequester.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.ucla.loni.pipeline.client.Requesters.RefreshAllTabs;
 
 import com.google.gwt.user.client.Window;
@@ -71,78 +70,4 @@ public class LONIDataRequester {
 		refreshResourceTabs();
 		refreshConfigurationTabs();
 	}
-=======
-package edu.ucla.loni.pipeline.client.Requesters.RefreshAllTabs;
-
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-import edu.ucla.loni.pipeline.client.Charts.LONI_Chart;
-import edu.ucla.loni.pipeline.client.Requesters.Configuration.RequestConfigurationXMLServiceAsync;
-import edu.ucla.loni.pipeline.client.Requesters.ResourceUsage.RequestResourceXMLServiceAsync;
-
-public class LONIDataRequester {
-	
-	private RequestResourceXMLServiceAsync reqResourceXMLService;
-	private RequestConfigurationXMLServiceAsync reqConfigurationXMLService;
-	private LONI_Chart memChart;
-	private LONI_Chart thrdChart;
-	
-	public LONIDataRequester(RequestResourceXMLServiceAsync reqResourceXMLService, RequestConfigurationXMLServiceAsync reqConfigurationXMLService, 
-			LONI_Chart memChart, LONI_Chart thrdChart) {
-		this.reqResourceXMLService = reqResourceXMLService;
-		this.reqConfigurationXMLService = reqConfigurationXMLService;
-		this.memChart = memChart;
-		this.thrdChart = thrdChart;
-	}
-	
-	public void refreshResourceTabs() {
-		
-		/** Resource Tabs */
-		reqResourceXMLService.getXMLData(new AsyncCallback<String>() {
-            @Override
-            public void onSuccess(String xmlData) {
-            	/** Workflow Tab */
-                
-                /** Users Tab */
-                
-                /** Memory Usage Tab */
-            	memChart.getChart().refreshChart(xmlData);
-                
-                /** Thread Usage Tab */
-            	thrdChart.getChart().refreshChart(xmlData);
-            	
-            	Window.alert("Resource Tabs refreshed successfully.");
-            }
-
-            @Override
-            public void onFailure(Throwable caught) {
-                Window.alert("Resource Tabs did not refresh successfully.");
-            }
-        });
-	}
-	
-	public void refreshConfigurationTabs() {
-		
-		/** Configuration Tabs */
-		reqConfigurationXMLService.getXMLData(new AsyncCallback<String>() {
-            @Override
-            public void onSuccess(String result) {
-            	/** Preferences Tab */
-                
-                Window.alert("Configuration Tabs refreshed successfully.");
-            }
-
-            @Override
-            public void onFailure(Throwable caught) {
-            	Window.alert("Configuration Tabs did not refresh successfully.");
-            }
-        });
-	}
-	
-	public void refreshTabs() {		
-		refreshResourceTabs();
-		refreshConfigurationTabs();
-	}
->>>>>>> branch 'master' of https://github.com/ppetrosyan/LONI-WebST.git
 }
