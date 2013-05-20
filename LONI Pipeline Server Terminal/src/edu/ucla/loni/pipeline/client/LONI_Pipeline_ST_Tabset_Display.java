9340ea8d5ab1ab190cfbda1183e2eb798d0a1b82
William Ho <arlonghk@gmail.com>
2013-05-19 19:14:42 -0700

    Update - Preference->access

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/LONI_Pipeline_ST_Tabset_Display.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/client/LONI_Pipeline_ST_Tabset_Display.java
@@ -23,6 +23,7 @@ import com.smartgwt.client.widgets.grid.ListGridField;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
 import com.smartgwt.client.data.DataSource;
 import com.smartgwt.client.types.AutoFitWidthApproach;
+import com.smartgwt.client.types.MultipleAppearance;
 import com.smartgwt.client.types.VerticalAlignment;
 import com.smartgwt.client.widgets.Button;
 import com.smartgwt.client.widgets.Canvas;
@@ -37,6 +38,8 @@ import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.form.fields.CheckboxItem;
 import com.smartgwt.client.widgets.form.fields.events.ChangeHandler;
 import com.smartgwt.client.widgets.form.fields.events.ChangeEvent;
+import com.smartgwt.client.widgets.form.fields.events.ChangedEvent;
+import com.smartgwt.client.widgets.form.fields.events.ChangedHandler;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.layout.HLayout;
@@ -851,6 +854,141 @@ public class LONI_Pipeline_ST_Tabset_Display {
 		VLayout layoutAccess = new VLayout();
 		
 		DynamicForm dynamicForm = new DynamicForm();
+		TextItem serverAdmin = new TextItem ("serverAdmin", "Server Admins");
+		dynamicForm.setFields(serverAdmin);getClass();
+		formatForm(dynamicForm);
+		layoutAccess.addMember(dynamicForm);
+		
+		//add line 
+		com.smartgwt.client.widgets.Label line = new com.smartgwt.client.widgets.Label("<hr>");
+		layoutAccess.addMember(line);
+		
+		com.smartgwt.client.widgets.Label label_1 = new com.smartgwt.client.widgets.Label("<b><font size=2>Directory Access Control</font></b>");
+		label_1.setSize("210px", "49px");
+		layoutAccess.addMember(label_1);
+		
+		DynamicForm dynamicForm2 = new DynamicForm();
+		dynamicForm2.setHeight("157px");
+		dynamicForm2.setTitleWidth(200);
+		
+		
+		final TextItem textItem_1 = new TextItem();
+		textItem_1.setDisabled(true);
+		textItem_1.setAlign(Alignment.LEFT);
+		textItem_1.setTitle("Controlled users");
+		textItem_1.setName("textbox_1");
+		final TextItem textItem_2 = new TextItem();
+		textItem_2.setDisabled(true);
+		textItem_2.setAlign(Alignment.LEFT);
+		textItem_2.setTitle("Controlled directories");
+		textItem_2.setName("textbox_2");
+		
+		final SelectItem selectItem = new SelectItem("newSelectItem_1", "Access control mode");
+		selectItem.setWidth(450);
+		selectItem.setDefaultToFirstOption(true);
+		selectItem.setAlign(Alignment.LEFT);
+		selectItem.addChangedHandler(new ChangedHandler() {
+			public void onChanged(ChangedEvent event) {
+		      
+	          //case0
+			  if ((selectItem.getValueAsString()).equals("0 -Executables: no restrictions. Remote File Browser: no restrictions."))
+		    	  {
+		    	    textItem_1.setTitle("Controlled users");
+		    	    textItem_2.setTitle("Controlled directories");
+		    	    textItem_1.setDisabled(true);
+			        textItem_2.setDisabled(true);
+		    	  }
+				
+			  //case1
+			  else if ((selectItem.getValueAsString()).equals("1 - Executables: restricted below. Remote File Browser: no restrictions"))
+		    	  {
+				  textItem_1.setDisabled(true);
+				     textItem_2.setDisabled(true);
+		    	    textItem_1.setTitle("Restricted users");
+		    	    textItem_2.setTitle("Restricted users only have access in these directories");
+		    	    textItem_1.setDisabled(false);
+			        textItem_2.setDisabled(false);
+		    	  }
+		      //case2
+			  else if ((selectItem.getValueAsString()).equals("2 - Executables: enabled. Remote File Browser: no restrictions"))
+		      {
+				  textItem_1.setDisabled(true);
+				     textItem_2.setDisabled(true);
+		    	  	textItem_1.setTitle("Special users with no restrictions");
+		    	    textItem_2.setTitle("Regular users only have access in these directories");
+		    	    textItem_1.setDisabled(false);
+			        textItem_2.setDisabled(false);
+		    	  }
+		      //case3
+			  else if ((selectItem.getValueAsString()).equals("3 - Executables: restricted below. Remote File Browser: restricted below"))
+		      { 
+				  textItem_1.setDisabled(true);
+				     textItem_2.setDisabled(true);
+		    	    textItem_1.setTitle("Restricted users");
+		    	    textItem_2.setTitle("Restricted users only have access in these directories");
+		    	    textItem_1.setDisabled(false);
+		    	    textItem_2.setDisabled(false);
+		      }
+		      //case4
+			  else if ((selectItem.getValueAsString()).equals("4 - Executables: enabled below. Remote File Browser: enabled below"))
+              {   
+				  textItem_1.setDisabled(true);
+				     textItem_2.setDisabled(true);
+            	     textItem_1.setTitle("Special users with no restrictions");
+			     textItem_2.setTitle("Regular users only have access in these directories");
+			     textItem_1.setDisabled(false);
+			     textItem_2.setDisabled(false);
+              }
+			  //case5
+			  else if ((selectItem.getValueAsString()).equals("5 - Executables: restricted below. Remote File Browser: same as Shell permissions"))
+			  {	
+				 textItem_1.setDisabled(true);
+			     textItem_2.setDisabled(true);
+			     textItem_1.setTitle("Restricted users");
+				 textItem_2.setTitle("Restricted users only have access in these directories");
+				 textItem_1.setDisabled(false);
+			     textItem_2.setDisabled(false);
+			  }
+		      //case6
+			  else if ((selectItem.getValueAsString()).equals("6 - Executables: enabled below. Remote File Browser: same as Shell permissions"))
+              {  
+				  textItem_1.setDisabled(true);
+			    textItem_2.setDisabled(true);
+            	textItem_1.setTitle("Special users with no restrictions");
+		        textItem_2.setTitle("Regular users only have access in these directories");
+		        textItem_1.setDisabled(false);
+		        textItem_2.setDisabled(false);
+              }
+		      //case7
+			  else if ((selectItem.getValueAsString()).equals("7 - Executables: same as Shell permissions. Remote File Browser: same as Shell"))
+		      {  
+		    	    textItem_1.setTitle("Controlled users");
+		        textItem_2.setTitle("Controlled directories");
+		        textItem_1.setDisabled(true);
+		        textItem_2.setDisabled(true);
+		      }
+			}
+		});
+		
+		selectItem.setMultipleAppearance(MultipleAppearance.GRID);
+		selectItem.setMultiple(false);
+		dynamicForm2.setFields(new FormItem[] { selectItem, textItem_1, textItem_2});
+		String[] sitesArray = {"0 -Executables: no restrictions. Remote File Browser: no restrictions", 
+				"1 - Executables: restricted below. Remote File Browser: no restrictions", 
+				"2 - Executables: enabled. Remote File Browser: no restrictions", 
+				"3 - Executables: restricted below. Remote File Browser: restricted below",
+				"4 - Executables: enabled below. Remote File Browser: enabled below",
+				"5 - Executables: restricted below. Remote File Browser: same as Shell permissions",
+				"6 - Executables: enabled below. Remote File Browser: same as Shell permissions",
+				"7 - Executables: same as Shell permissions. Remote File Browser: same as Shell"};
+		
+		selectItem.setValueMap(sitesArray);
+		formatForm(dynamicForm2);
+		layoutAccess.addMember(dynamicForm2);
+		
+		//add line 
+		com.smartgwt.client.widgets.Label line2 = new com.smartgwt.client.widgets.Label("<hr>");
+		layoutAccess.addMember(line2);
 		
 		final SpinnerItem spinnerItem_1 = new SpinnerItem("si_usrpcfreeslots", "One user cannot take more than");
 		spinnerItem_1.setValue(50);
@@ -876,6 +1014,7 @@ public class LONI_Pipeline_ST_Tabset_Display {
 		CanvasItem canvasItem_1 = new CanvasItem("ci_usrbjbmngmt", "User-Based Job Management");
 		canvasItem_1.setCanvas(dynamicForm_1);
 		
+		DynamicForm dynamicForm3 = new DynamicForm();
 		final SpinnerItem spinnerItem_2 = new SpinnerItem("si_maxwrkflpu", "Maximum active (running & paused) workflows per user");
 		spinnerItem_2.setValue(20);
 		spinnerItem_2.setMin(0);
@@ -897,16 +1036,13 @@ public class LONI_Pipeline_ST_Tabset_Display {
 		CanvasItem canvasItem_2 = new CanvasItem("ci_wrkflmngmt", "Workflow Management");
 		canvasItem_2.setCanvas(dynamicForm_2);
 		
-		dynamicForm.setFields(new FormItem[] { new TextItem("ti_srvadm", "Server Admins"), 
-				   			  				   new TextItem("ti_cntrldusrs", "Controlled users"), 
-				   			  				   new TextItem("ti_cntrlddirs", "Controlled directories"), 
-				   			  				   canvasItem_1, 
-				   			  				   spinnerItem_1, 
-				   			  				   canvasItem_2, 
-				   			  				   spinnerItem_2});
-		formatForm(dynamicForm);
-		layoutAccess.addMember(dynamicForm);
-
+		dynamicForm3.setFields(canvasItem_1, 
+				   			  	spinnerItem_1, 
+				   			  	canvasItem_2, 
+				   			  	spinnerItem_2);
+		
+		formatForm(dynamicForm3);
+		layoutAccess.addMember(dynamicForm3);
 		tabAccess.setPane(layoutAccess);
 		return tabAccess;
 	}
