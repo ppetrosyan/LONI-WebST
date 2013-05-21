7389e88704867965187188abc7047448bd80049c
Jared <lindblom@cs.ucla.edu>
2013-05-21 16:30:47 -0700

    Refactored FileUpload Servlet, switched to fileitemstream to support appspot.

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/server/Savers/Configuration/SaveResourceCSVServlet.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/server/Savers/Configuration/SaveResourceCSVServlet.java
@@ -2,20 +2,7 @@ package edu.ucla.loni.pipeline.server.Savers.Configuration;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.nio.channels.Channels;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
