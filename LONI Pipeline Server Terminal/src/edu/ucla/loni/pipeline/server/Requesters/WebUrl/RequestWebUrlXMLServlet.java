e51175e8ed20898127c15dfaf4ae0f986eaff42c
Deepak Ananth Rama <maildeepakar@gmail.com>
2013-05-26 13:40:58 -0700

    framework for weburl

--- LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/server/Requesters/WebUrl/RequestWebUrlXMLServlet.java
+++ LONI Pipeline Server Terminal/src/edu/ucla/loni/pipeline/server/Requesters/WebUrl/RequestWebUrlXMLServlet.java
@@ -1,32 +1,87 @@
 package edu.ucla.loni.pipeline.server.Requesters.WebUrl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import edu.ucla.loni.pipeline.client.Requesters.WebUrl.RequestWebUrlXMLService;
 import edu.ucla.loni.pipeline.server.Utilities.DatastoreUtils;
-import edu.ucla.loni.pipeline.server.Utilities.ResponseBuilder;
+import edu.ucla.loni.pipeline.server.Utilities.WebUrlResponseBuilder;
 
 public class RequestWebUrlXMLServlet extends RemoteServiceServlet implements RequestWebUrlXMLService {
 
 	private static final long serialVersionUID = 5448261063802349760L;
-	//private Key xmlConfigurationKey;
+	private Key xmlResourceKey, xmlConfigurationKey;
+	WebUrlResponseBuilder response;
 
 	public RequestWebUrlXMLServlet() {
-		//xmlConfigurationKey = KeyFactory.createKey("XMLType", "ConfigurationData");
+		xmlConfigurationKey = KeyFactory.createKey("XMLType", "ConfigurationData");
+		xmlResourceKey = KeyFactory.createKey("XMLType", "ResourceData");
+		response = new WebUrlResponseBuilder();
 	}
 
 	@Override
 	public String getXML(String url) {
-		String xmlData = "test data returned";
+		String xml = null;
 		
-		//ResponseBuilder respBuilder = new ResponseBuilder();
-		//xmlData = DatastoreUtils.readXMLFileFromBlobStore(xmlConfigurationKey, respBuilder);
-		
-		// Debug
-		//System.out.println(respBuilder.getRespMessage());
-		
-		return xmlData;
+		try {
+			  	URL u = new URL(url);
+			  	HttpURLConnection huc = (HttpURLConnection) u.openConnection();
+			  	huc.setRequestMethod("GET");
+			  	huc.setDoOutput(false);
+			  	int status = huc.getResponseCode();
+			  	if (status != 200)
+			  		throw (new MalformedURLException());
+			  	InputStream in = huc.getInputStream();
+			  	
+			  	BufferedReader d = new BufferedReader(new InputStreamReader(in));
+			  	xml = d.readLine();
+			  	if(xml.length() == 0)
+			  		return null;
+			  	
+			  	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			  	DocumentBuilder builder = factory.newDocumentBuilder();
+			  	Document document = builder.parse(in);
+				document.getDocumentElement().normalize();
+
+				String rootTag = document.getDocumentElement().getNodeName();
+				if(rootTag.equalsIgnoreCase("LONIConfigurationData")) {
+					//DatastoreUtils.writeXMLFileToBlobStore(document, xmlConfigurationKey, null);//response);
+				}
+				else if(rootTag.equalsIgnoreCase("LONIResourceData")){
+					//DatastoreUtils.writeXMLFileToBlobStore(document, xmlResourceKey, null);//response);
+				}
+				else {
+					//response.appendRespMessage("Invalid file format, incorrect root tags founds");
+					//xml = null;
+				}
+		} 
+		catch (IOException e) {
+			response.appendRespMessage("Error on file retrieval, try again");
+			xml = null;
+		} 
+		catch (ParserConfigurationException | SAXException e) {
+			response.appendRespMessage("Error in parsing retrieved file");
+			//xml = null;
+		}
+
+		//if (xml != null)
+			//response.setXml(xml);
+		return xml; //response;
 	}
 }
